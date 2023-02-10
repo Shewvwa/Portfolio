bd9ebf0948c0c67a79b331aef3e3bd11b43d82ed
Shevchuk Michael <mishashev@icloud.com>
2023-02-10 12:18:53 +0300

    3

--- js/script.js
+++ js/script.js
@@ -22,29 +22,6 @@ counter.forEach((item, i) => {
 })
 
 
-let isScroll = 0, // доп. проверка
-    targetScroll = 400; // расстояние до действия / в px
-    
-$(window).on('scroll', function(){
-  if(isScroll === 0 && $(this).scrollTop() >= targetScroll) {
-    isScroll = 1;
-    $('.sidepanel_text ').css('color', 'black',);
-    $('.sidepanel_divider ').css('background-color', 'black');
-    $('.sidepanel_svg path ').css('fill', 'black');
-    console.info('change 1');
-  } else if(isScroll === 1 && $(this).scrollTop() < targetScroll) {
-    isScroll = 0;
-    $('.sidepanel_text').css('color', 'white');
-    $('.sidepanel_divider ').css('background-color', 'white');
-    $('.sidepanel_svg path ').css('fill', 'white');
-    console.info('change 0');
-  }
-});
 
-$(window).scroll(function(){
-    if($(this).scrollTop() > 900) {
-        $('.pageup').fadeIn();
-    } else {
-        $('.pageup').fadeOut();
-    }
-});
+
+
