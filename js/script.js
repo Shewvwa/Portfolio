77878fce9eda4b7fcf67047853705f44a9989422
Shevchuk Michael <mishashev@icloud.com>
2023-02-17 10:14:33 +0300

    7

--- js/script.js
+++ js/script.js
@@ -2,20 +2,24 @@ const hamburger = document.querySelector('.hamburger'),
       menu = document.querySelector('.menu'),
       close = document.querySelector('.menu_close');
       promoactive = document.querySelector('.promo');
+      sidepanel = document.querySelector('.sidepanel');
 
 hamburger.addEventListener('click', ()=>{
     menu.classList.add('active');
     promoactive.classList.add('active');
+    sidepanel.classList.add('active');
 });
 
 close.addEventListener('click', ()=>{
     menu.classList.remove('active');
     promoactive.classList.remove('active');
+    sidepanel.classList.remove('active');
 });
 
 menu.addEventListener('click', ()=>{
     menu.classList.remove('active');
     promoactive.classList.remove('active');
+    sidepanel.classList.remove('active');
 });
 
 
